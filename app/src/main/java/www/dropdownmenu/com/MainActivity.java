be95b4a32ccae276a71e5269b15a4ff4cdc32cc0
turangokdemir <turangokdemir9@gmail.com>
2022-03-07 21:56:00 +0300

    Exposed DropDown Menu Android Studio

---
+++ app/src/main/java/www/dropdownmenu/com/MainActivity.java
@@ -0,0 +1,36 @@
+package www.dropdownmenu.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String [] items = {"Java", "Python","React Native", "Flutter"};
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> adapterItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        autoCompleteTextView = findViewById(R.id.autoComplete);
+        adapterItems = new ArrayAdapter<String>(this,R.layout.list_item,items);
+        autoCompleteTextView.setAdapter(adapterItems);
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+
+                Toast.makeText(getApplicationContext(),"Kategori:" +item, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
